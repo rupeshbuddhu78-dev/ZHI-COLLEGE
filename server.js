a29d26d0bb65a632fbd98ea7a6287b3dc165bd83
Rupesh kumar <rupeshbuddhu78@gmail.com>
2026-03-27 23:27:57 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const nodemailer = require('nodemailer'); // 🔴 NAYA PACKAGE EMAIL KE LIYE
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// --- MIDDLEWARE ---
+// --- MIDDLEWARE --
 app.use(cors());
 app.use(express.json()); 
 app.use(express.static(path.join(__dirname, 'public')));
