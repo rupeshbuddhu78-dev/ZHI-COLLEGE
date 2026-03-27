49d933c01a2d1d8c94ab05dd7c38b711b71db65a
Rupesh kumar <rupeshbuddhu78@gmail.com>
2026-03-28 00:17:30 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
-const nodemailer = require('nodemailer'); // 🔴 NAYA PACKAGE EMAIL KE LIYE
+const nodemailer = require('nodemailer'); // 🔴 NAYA PACKAGE EMAIL KE LIYe
 
 const app = express();
 const PORT = process.env.PORT || 5000;
