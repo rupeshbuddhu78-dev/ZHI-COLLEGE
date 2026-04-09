17541ce224a1ce47c5dcce34d851108d8a9d967d
Rupesh kumar <rupeshbuddhu78@gmail.com>
2026-04-09 17:58:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -83,13 +83,18 @@ mongoose.connect(process.env.MONGO_URI || "mongodb+srv://rupeshdatabase:rupeshku
 
 // --- 4. EMAIL SETUP ---
 // --- NODEMAILER SECURE CONFIGURATION ---
+// --- FINAL NODEMAILER CONFIGURATION ---
 const transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
-    port: 465,
-    secure: true,
+    port: 587,                   // 🔴 Port 465 hata kar 587 kar diya (Free servers pe ye block nahi hota)
+    secure: false,               // 🔴 Port 587 ke liye isko false rakhna padta hai
+    requireTLS: true,            // 🔴 Lekin connection secure rahega is line se
     auth: {
-        user: '8651142739rupesh@gmail.com',
-        pass: 'qkyl mbcm xnux iteb'
+        user: '8651142739rupesh@gmail.com',  // Tera naya email
+        pass: 'qkylmbcmxnuxiteb'             // Tera 16-digit password
+    },
+    tls: {
+        rejectUnauthorized: false
     }
 });
 
