0300bdd0c2d62e70b5232afe87802c3ff280c148
Rupesh kumar <rupeshbuddhu78@gmail.com>
2026-06-15 08:11:35 +0530

    Update transaction.js

--- models/transaction.js
+++ models/transaction.js
@@ -1,1 +1,7 @@
-
+const transactionSchema = new mongoose.Schema({
+    receiptNo: { type: String, unique: true },
+    studentId: { type: mongoose.Schema.Types.ObjectId, ref: 'Student' },
+    date: Date, mode: String, amount: Number, feeHeadName: String, remarks: String,
+    payerMobile: String
+}, { timestamps: true });
+const Transaction = mongoose.model('Transaction', transactionSchema);
