d841783d569f9392c8a03a058d4d23bfefdd33e1
Rupesh kumar <rupeshbuddhu78@gmail.com>
2026-06-15 08:19:24 +0530

    Update and rename Routine.js to routine.js

---
+++ models/routine.js
@@ -0,0 +1,15 @@
+// ROUTINE SCHEMA
+const routineSchema = new mongoose.Schema({
+    course: { type: String, required: true },
+    semester: { type: String, required: true },
+    section: { type: String, required: true },
+    subject: { type: String, required: true },
+    teacherId: { type: mongoose.Schema.Types.ObjectId, ref: 'Staff', required: true },
+    teacherName: { type: String, required: true },
+    date: { type: String, required: true },
+    dayOfWeek: { type: String, required: true },
+    startTime: { type: String, required: true },
+    endTime: { type: String, required: true },
+    roomNumber: { type: String }
+}, { timestamps: true });
+const Routine = mongoose.model('Routine', routineSchema);
