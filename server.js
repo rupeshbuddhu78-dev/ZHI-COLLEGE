b820bb2dbd624e67c7ad37c5f320cbc8418682e0
Rupesh kumar <rupeshbuddhu78@gmail.com>
2026-06-15 07:39:37 +0530

    Update server.js

--- server.js
+++ server.js
@@ -160,16 +160,7 @@ const expenseSchema = new mongoose.Schema({
 }, { timestamps: true });
 const Expense = mongoose.model('Expense', expenseSchema);
 
-// NOTICE SCHEMA
-const noticeSchema = new mongoose.Schema({
-    title: { type: String, required: true },
-    message: { type: String, required: true },
-    priority: { type: String, default: "info" },
-    audience: [{ type: String }],
-    fileUrl: { type: String, default: "" },
-    postedBy: { type: String, default: "Director Office" }
-}, { timestamps: true });
-const Notice = mongoose.model('Notice', noticeSchema);
+
 
 
 
