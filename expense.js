cf7640437e15e5fb902a38d063c0f61f90a7fe38
Rupesh kumar <rupeshbuddhu78@gmail.com>
2026-06-15 08:12:45 +0530

    Delete expense.js

--- expense.js
+++
@@ -1,5 +0,0 @@
-const expenseSchema = new mongoose.Schema({
-    voucherNo: { type: String, unique: true }, category: String, date: Date,
-    mode: String, amount: Number, description: String
-}, { timestamps: true });
-const Expense = mongoose.model('Expense', expenseSchema);
