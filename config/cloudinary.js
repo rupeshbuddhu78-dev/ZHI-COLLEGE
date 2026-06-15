d8a7432250b73674d9f046b48b45dcea2fe161b2
Rupesh kumar <rupeshbuddhu78@gmail.com>
2026-06-15 07:58:04 +0530

    Update cloudinary.js

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,1 +1,7 @@
-
+// --- 2. CLOUDINARY CONFIGURATION ---
+cloudinary.config({
+    cloud_name: 'dzbpiv7ds',
+    api_key: '812196161439545',
+    api_secret: 'gWdxF2wJvGeuMqvpDgmNogS2pdY',
+    secure: true
+});
