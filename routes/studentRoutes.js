250776ee077abcbd4a816e8bd96c089dcfc206eb
Rupesh kumar <rupeshbuddhu78@gmail.com>
2026-06-15 09:47:15 +0530

    Update studentRoutes.js

--- routes/studentRoutes.js
+++ routes/studentRoutes.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const studentController = require('../controllers/studentController');
-const upload = require('../config/multer'); // Tumhara multer import (Photo ke liye)
+
+// 🔥 Naya Multer Import (Sirf Profile upload wala uploader nikala hai)
+const { uploadProfile } = require('../config/multer'); 
 
 router.post('/add-student', studentController.addStudent);
 router.get('/', studentController.getStudents);
 router.put('/:id', studentController.updateStudent);
 router.delete('/:id', studentController.deleteStudent);
-router.post('/upload-photo/:id', upload.single('profileImage'), studentController.uploadPhoto);
+
+// Yahan 'uploadProfile' use kiya hai
+router.post('/upload-photo/:id', uploadProfile.single('profileImage'), studentController.uploadPhoto);
 
 module.exports = router;
